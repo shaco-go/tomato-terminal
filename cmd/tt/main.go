2449f2bbcf11cf41e85e1aea462a96dd3eece5b0
18685850590 <liujunyi_coder@163.com>
2026-03-13 18:40:46 +0800

    fix

--- cmd/tt/main.go
+++ cmd/tt/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/shaco-go/tomato-terminal/bootstrap"
 	"github.com/shaco-go/tomato-terminal/config"
 	"github.com/shaco-go/tomato-terminal/page"
@@ -16,6 +18,9 @@ func main() {
 			zap.L().Fatal("panic", zap.Any("err", err))
 		}
 		config.Conf.Flush()
+		// 清除并重置光标
+		fmt.Print("\033[3A") // 向上3行
+		fmt.Print("\033[J")  // 清除从光标到屏幕底部的所有内容
 	}()
 
 	_, err := tea.NewProgram(page.New()).Run()
